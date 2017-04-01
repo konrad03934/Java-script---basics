843999ec478fa21a69d2abdfcfa72fa7b514a70d
Konrad Kowalczyk <konradkowalczyk89@gmail.com>
2017-04-01 11:05:41 +0200

    tablice

---
+++ js/zmienne-tablicowe.js
@@ -0,0 +1,18 @@
+var imiona = new Array();
+//9 okresla rozmiar tablicy- czyli tyle komurek ma zadeklarowane
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateusz', 'Michał', 'Krzysiek', 'Sergio');
+
+imionaChlopakow[2];
+
+console.log(imiona);
+console.log(imionaChlopakow[2]);
+console.log(imionaChlopakow);
+console.log(imionaStudentow);
+
+// lub inny zapis tego samego
+var wiek = [];
+var wiekStudentow = [20, 30, 55];
+
+console.log(wiek);
+console.log(wiekStudentow);
