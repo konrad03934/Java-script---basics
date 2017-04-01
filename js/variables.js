c587eef8c330683f5fa9a9db3f94cd5b9843ca8e
Konrad Kowalczyk <konradkowalczyk89@gmail.com>
2017-04-01 09:39:34 +0200

    zmienne

---
+++ js/variables.js
@@ -0,0 +1,13 @@
+var imie = "Konrad";
+var imieDemskie = "Kaja";
+var wiek = 37;
+var kolor ="czerwony";
+
+
+console.log(imie);
+console.log(imieDemskie);
+console.log(wiek);
+console.log(kolor);
+
+wiek = 27;
+console.log(wiek);
