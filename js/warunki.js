7363e7851b96cbedf8b31d3f850f69504bc0cac1
Konrad Kowalczyk <konradkowalczyk89@gmail.com>
2017-04-01 11:56:22 +0200

    switch

---
+++ js/warunki.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var wzrostMateusz = 185;
+var wzrostKaji = 190;
+var result;
+
+/*if (wzrostMateusz > wzrostKaji) {
+	console.log("mateusz jes twyższy");
+} else {
+	console.log("Kaja jest wyższa");
+}*/
+
+//moj przyklad nie wiem czy dobry
+switch (result) {
+		case wzrostKaji < wzrostMateusz:
+		console.log("mateusz jest wyższy");
+		break;
+	default:
+		console.log("Kaja jest wyższa");
+}
+
+var kolor = "zielony";
+
+switch (kolor) {
+	case "zielony":
+			console.log("jest git");
+			break;
+		case "rudy":
+			console.log("ten jest rudy a nie zielony");
+			break;
+		default:
+			console.log("jakiś pedalski");
+}
+
+
+swich (true) {
+	case wzrostKaji > wzrostMateusz;
+	console.log("kaja jest wyższa");
+	break;
+	case wzrostKaji == wzrostMateusz;
+	console.log()
+}
