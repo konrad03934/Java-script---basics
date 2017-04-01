42f90e0dfa6fc725a05e576bcaafffdd0d87daca
Konrad Kowalczyk <konradkowalczyk89@gmail.com>
2017-04-01 10:43:57 +0200

    ify i i zmienne

---
+++ js/typy-danych.js
@@ -0,0 +1,41 @@
+"use stict"
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata +
+	premia;
+
+console.log(calkowitaWyplata);
+
+
+//dodawanie dwóch stringów
+var wyplataSting = "jakas tam";
+var premiaSting = "nastepna";
+var calkowitaWyplataSting;
+
+calkowitaWyplataSting = wyplataSting +
+	premiaSting;
+
+console.log(calkowitaWyplataSting);
+
+//warunki logiczne
+var czyJestSmog = true;
+
+if (czyJestSmog) {
+	console.log("jest Smog");
+} else {
+	console.log("nie ma skogu");
+}
+
+//
+var niezdefiniowanaZmienna;
+var nullowanaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowanaZmienna);
+
+var imieStudenta = "Michał";
+console.log(imieStudenta.indexOf("c"));
+						
